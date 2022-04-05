c5bd01ae1480cf3aab7c72c2847ee58d9648c096
shakilwebmaster <mdshakil102036@gmail.com>
2022-04-05 14:39:27 +0600

    jangkar mahbub course prectice

---
+++ app.js
@@ -0,0 +1,116 @@
+// var teaLine = [ 'kalam', 'salam', 'balam'];
+// teaLine.unshift('palam');
+// console.log(teaLine)
+// convert minutes to second
+// const secondFun = (minutes) =>{
+//     const seconds = minutes * 60;
+//     console.log(seconds)
+// }
+
+// secondFun(5)
+// console.log(toHex(1))
+
+
+//   const name = 'shakil Hossain';
+//   console.log(name.length);
+//   console.log(name.length>0)
+//   console.log(name.length>18)
+
+//   const leapYear = 2030
+// //   console.log(leapYear)
+
+//   if (leapYear % 4 == 0){
+//       console.log(leapYear + "is not leapYear")
+//   }
+//   else{
+//       console.log(leapYear + " is leapYear")
+//   }
+
+//   function fibonacci(n){
+//       if(n==0){
+//         return [0]
+//       }
+//       else if(n==1){
+//         return [0, 1]
+//       }
+//       else {
+//         var fibo = fibonacci(n-1);
+//         var nextElement = fibo[n-1] + fibo[n-2];
+//         fibo.push(nextElement);
+//         return fibo;
+//       }
+//     }
+
+//     fibonacci(0)
+
+//swap problem
+
+// var a = 5;
+// var b = 7;
+
+// console.log("before swap : a = "+ a, "b= ", b);
+
+// var temp ;
+
+// temp =a
+// a=b;
+// b = temp;
+// console.log("after swap : a = "+ a, "b= ", b);
+
+
+//array max
+
+// var marks = [45, 58, 69, 85, 46, 97];
+
+// var max = marks[0];
+
+// for(var i = 0; i<= marks.length; i++){
+//     var element = marks[i];
+//     if(element> max){
+//         max = element;
+//     }
+// }
+
+// console.log("height value is : ", max)
+
+
+
+
+// function sumArray (numbers){
+
+//         var sum = 0;
+        
+//         for (var i = 0; i<numbers.length; i++){
+//             var element = numbers[i];
+//             sum = sum + element;
+//         }
+//     return sum
+// }
+
+// var numbers = [45, 58, 69, 85, 46, 97];
+// var result = sumArray(numbers);
+// console.log("Total number is: ", result);
+
+// var numbers = [544,54,87,871,544,64];
+// var result = sumArray(numbers);
+// console.log("Total summition is : ", result);
+
+
+
+//duplicate array remove;
+
+var names = ["shakil", "kamal", "jamal", 
+"roni", "rakib ", "suhad","jamal", "roni", "rakib ", "suhad", "kalam"];
+
+var uniqueName = [];
+
+for(var i = 0; i<names.length; i++){
+    var element = names[i];
+    var index = uniqueName.indexOf(element);
+    if(index == -1){
+        uniqueName.push(element)
+    }
+}
+
+console.log(uniqueName)
+
