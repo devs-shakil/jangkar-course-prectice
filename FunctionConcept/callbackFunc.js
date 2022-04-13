79ee2560ee826e85427e91d0f419b59a9154c0c6
shakilwebmaster <mdshakil102036@gmail.com>
2022-04-13 22:01:06 +0600

    Callback function and arguments prectice

---
+++ FunctionConcept/callbackFunc.js
@@ -0,0 +1,26 @@
+// function person(name, age, task){
+//     console.log("Hello ", name);
+//     console.log("Your age", age);
+//     task();
+// }
+
+// person("Shakil Hossain", 24, work  )
+
+// function work(){
+//     console.log("hand wash");
+
+// }
+// function doThis(){
+//     console.log("hello suhad do this ");
+// }
+// console.log("------------------")
+
+// person("suhad mia", 25, doThis);
+
+// console.log("--------------")
+// function Hello (){
+//     console.log("this is callBack function");
+// }
+// person("Rakib Hossain", 26, Hello);
+
+// //task is callback function
---
+++ FunctionConcept/function.js
@@ -0,0 +1,26 @@
+// function evening(num){
+//     let result;
+//     if(num % 2 == 0){
+//         result = num;
+//     }else{
+//         result =  num * 2;
+//     }
+//     return result;
+// }
+// const result = evening(13);
+// // console.log(result);
+
+
+// function evenifyWithArray (nums){
+//     let output = []
+//     for(let i = 0; i<nums.length; i++){
+//         let num = nums[i];
+//         let outputArray = evening(num);
+//         output.push(outputArray)
+//     }
+//     return output;
+// }
+// let nums = [1,2,3,4,5,6,7,8,9];
+
+// let isOutput = evenifyWithArray(nums);
+// console.log(isOutput)
