1ef0901e7026b117e5a4d53b93c237eb2f3fc80b
shakilwebmaster <mdshakil102036@gmail.com>
2022-09-11 11:57:25 +0600

    map, reduce, filter method practice

---
+++ map-filter.js
@@ -0,0 +1,67 @@
+// Problem 1 for loop
+/* const number = [2,3,4,5,6,7,8,9];
+const output = [];
+
+for(let i = 0; i<number.length; i++){
+    let elements = number[i];
+    const result = elements * elements;
+    output.push(result);
+}
+console.log(output) */
+
+
+
+//problem 2 for loop
+
+/* const num = [1,3,5,7,9];
+const output = [];
+for(let i = 0; i<num.length; i++){
+    const element = num[i];
+    const result = element * element;
+    let sumIs = 0;
+    output.push(result);
+}
+console.log(output);
+ */
+
+
+// Problem 3 array.map()
+//1,2,3,4,5,6 square
+/*  const number = [1,2,3,4,5,6]
+ const square = (element) =>{
+    return element * element;
+ }
+const result = number.map(square);
+console.log(result); */
+
+
+//problem 4 array.map way 2
+//1,2,3,4,5,6 square
+/* const number = [1,2,3,4,5,6]
+const square = number.map(element => element * element);                //element => element * element
+console.log(square);   */                                                  /* const square = (element)=>{ return element*element}*/
+
+
+//Problem 5 .filter()
+//sum every possitive number of (1,-5,-26,50,-4,36,20,26,-29,11,-45)
+/* 
+const number = [1,-5,-26,50,-4,36,20,26,-29,11,-45];
+ const sum = number.filter(element => element>0).reduce((priviousValue, currentValue) => {
+    return priviousValue + currentValue;
+},0);
+console.log(sum); 
+*/
+
+
+
+ //another way
+ /* const number = [1,-5,-26,50,-4,36,20,26,-29,11,-45];
+ const posstiveNumber = number.filter(element => element > 0);
+ function readyToSum (priviousValue, currentValue){
+    return priviousValue + currentValue;
+ };
+ const sum = posstiveNumber.reduce(readyToSum);
+ console.log(sum); */
+
+
+ 
