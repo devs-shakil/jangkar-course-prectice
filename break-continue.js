ce1bbda15984d23ce375493930a41579a01413fa
shakilwebmaster <mdshakil102036@gmail.com>
2022-09-13 19:50:01 +0600

    somthing added

---
+++ break-continue.js
@@ -0,0 +1,20 @@
+/* const nums = [1,2,3,4,5,6,7,8,9];
+
+for(let i = 0; i<nums.length; i++){
+    console.log(nums[i]);
+    if(nums[i]>5){
+        break;  // break 
+    }
+};
+
+console.log("-----------------")
+
+const number = [1,2,-3,4,5,6,-7,8,-9];
+for(let i = 0; i<number.length; i++){
+    
+
+    if(number[i]<0){
+        continue;
+    }
+    console.log(number[i]);
+} */
---
+++ closure.js
@@ -0,0 +1,29 @@
+/* const stopWatch = () =>{
+    let count = 0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+console.log("serve no 1")
+const clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+
+console.log("serve no 2");
+const clock2 = stopWatch();
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+
+console.log("serve 1 and 2 ");
+console.log(clock1());
+console.log(clock2());
+console.log(clock1()); */
