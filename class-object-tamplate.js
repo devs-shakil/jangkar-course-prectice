01c48e6c85605194cfa446362515720bb32db038
shakilwebmaster <mdshakil102036@gmail.com>
2022-09-21 21:57:31 +0600

    class object tamplate added

---
+++ class-object-tamplate.js
@@ -0,0 +1,14 @@
+class Person{
+    constructor (firstName, lastName, age, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;    
+        this.salary = salary;
+    }
+};
+
+const person1 = new Person('Shakil', 'Hossain', 24, 20000);
+const person2 = new Person('Rakib', 'Hossain', 25, 20000);
+
+console.log(person1);
+console.log(person2)
